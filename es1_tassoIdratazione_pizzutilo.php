cd8f64420915211060a0b84ff08bd3c414c5ecc3
Tommaso Pizzutilo <tommaso.pizzutilo@gmail.com>
2024-09-20 10:52:36 +0200

    es1_TassoIdratazione

---
+++ es1_tassoIdratazione_pizzutilo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasso di idratazione</title>
+</head>
+<body>
+    <?php
+        $peso = 65;
+        $tassoIdr;
+
+
+
+        echo "<h1>Tommaso Pizzutilo</h1>";
+        $tassoIdr = $peso * 30;
+
+        echo "<p>Il tasso di idratazione di una persona che pesa $peso kg è di $tassoIdr </p>"
+
+
+    ?>
+
+
+    
+</body>
+</html>
